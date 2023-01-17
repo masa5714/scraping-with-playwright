848384e798a7becb19f0451c572df0c1dbc699d1
masa5714 <masa5714921743@gmail.com>
2023-01-17 18:44:19 +0900

    responseのサンプルを調整

--- sample/response.ts
+++ sample/response.ts
@@ -1,22 +1,19 @@
 import { Scraping } from "./../src/index";
 
 const watchFontManifest = async (scraping: Scraping) => {
-  scraping.watchResponse({
-    domain: "flutter-canvas-89807.web.app",
-    path: "FontManifest.json",
-    status: 200,
-    contentType: "application/json",
-    successFunction: async (page, response) => {
-      // 成功したときの処理
+  scraping.watchResponse(
+    {
+      domain: "flutter-canvas-89807.web.app",
+      path: "FontManifest.json",
+      status: 200,
+      contentType: "application/json",
+    },
+    async (page, response, status) => {
+      console.log(await page.title());
       console.log(await response.text());
-    },
-    errorFunction: async (page, response) => {
-      // 失敗したときの処理
-      console.log("failed");
-      // await page.waitForLoadState("domcontentloaded");
-      // await page.reload();
-    },
-  });
+      console.log(status);
+    }
+  );
 };
 
 (async () => {
